fcbcaf119465c01abee11a6adda38bd2b0bb67bc
janae.wel <janae.wel@gmail.com>
2022-08-31 12:18:24 -0400

    bonus comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./api/user-routes');
+const userRoutes = require('.user-routes');
 const projectRoutes = require('./project-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
